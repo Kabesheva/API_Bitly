5e9e7ee0ef0c87ebcfea118fba0b6866a977516d
Kabesheva <50301818+Kabesheva@users.noreply.github.com>
2020-02-28 01:40:35 +0300

    Update main.py
    исправлен регистр букв

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import requests
 from dotenv import load_dotenv
 
 
-def shorten_link(BITLY_TOKEN, user_input_url):
+def shorten_link(token, user_input_url):
     link = 'https://api-ssl.bitly.com/v4/shorten'
-    HEADERS = {"Authorization": "Bearer " + BITLY_TOKEN}
+    headers = {"Authorization": "Bearer " + token}
     payload = {"long_url": user_input_url}
-    response = requests.post(link, json=payload, headers=HEADERS)
+    response = requests.post(link, json=payload, headers=headers)
     response.raise_for_status()
     bitlink = response.json()['id']
     return bitlink
 
 
-def count_clicks(BITLY_TOKEN, bitlink):
+def count_clicks(token, bitlink):
     url_clicks = (f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary')
-    HEADERS = {"Authorization": "Bearer " + BITLY_TOKEN}
+    headers = {"Authorization": "Bearer " + token}
     params = {'unit': 'day', 'units': '-1'}
-    response = requests.get(url_clicks, params=params, headers=HEADERS)
+    response = requests.get(url_clicks, params=params, headers=headers)
     response.raise_for_status()
     clicks = response.json()['total_clicks']
     return clicks
@@ -27,18 +27,18 @@ def count_clicks(BITLY_TOKEN, bitlink):
 
 def main():
     load_dotenv()
-    BITLY_TOKEN = os.getenv('BITLY_TOKEN')
+    token = os.getenv('BITLY_TOKEN')
     parser = argparse.ArgumentParser(description='Сокращаем ссылку через Bitly.com')
     parser.add_argument('user_input_url', help='Введите ссылку')
     args = parser.parse_args()
     user_input_url = args.user_input_url
     try:
         if user_input_url.startswith('bit.ly'):
-            print('Кликов по ссылке:', count_clicks(BITLY_TOKEN, user_input_url))
-            return count_clicks(BITLY_TOKEN, user_input_url)
+            print('Кликов по ссылке:', count_clicks(token, user_input_url))
+            return count_clicks(token, user_input_url)
         else:
-            print('Короткая ссылка:', shorten_link(BITLY_TOKEN, user_input_url))
-            return shorten_link(BITLY_TOKEN, user_input_url)
+            print('Короткая ссылка:', shorten_link(token, user_input_url))
+            return shorten_link(token, user_input_url)
     except requests.exceptions.HTTPError as error:
         print(f'\nНеверная ссылка!\nОшибка\n{error}.')
 
